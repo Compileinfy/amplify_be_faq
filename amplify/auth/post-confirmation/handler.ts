553304538f8c8d55a60536e14a0d46e059c6fcc5
Saicharan <saicharan.anumasula@compileinfy.com>
2025-06-23 12:40:11 +0530

    Assigned user to profile group

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -4,19 +4,40 @@ import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
 import { env } from "$amplify/env/post-confirmation";
+import { CognitoIdentityProviderClient, AdminAddUserToGroupCommand } from "@aws-sdk/client-cognito-identity-provider";
+import { profileGroups } from "../profileGroups";
 
-const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(
-  env
-);
+
+const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(env);
 
 Amplify.configure(resourceConfig, libraryOptions);
 
-const client = generateClient<Schema>();
+ const client = generateClient<any>();
+ const cognitoClient = new CognitoIdentityProviderClient();
+
+
+ async function addUserToGroup(username: string, userPoolId: string): Promise<void> {
+    const command = new AdminAddUserToGroupCommand({
+        GroupName: profileGroups.USER,
+        Username: username,
+        UserPoolId: userPoolId
+    });
+
+    const response = await cognitoClient.send(command);
+    console.log("User added to Cognito group", { requestId: response.$metadata.requestId });
+}
 
 export const handler: PostConfirmationTriggerHandler = async (event) => {
-  await client.models.userModel.create({
-      email: event.request.userAttributes.email,
-  });
 
+  const userId = event.request.userAttributes.sub;
+  const email = event.request.userAttributes.email;
+  const username = event.userName;
+  const userPoolId = event.userPoolId;
+  console.log("post conformation event:",{ userId,email, username, userPoolId });
+  try {
+        await addUserToGroup(username, userPoolId);
+    } catch (groupError) {
+        console.log("Failed to add user to group", groupError, 'error');
+  }
   return event;
 };
